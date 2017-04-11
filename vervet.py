149c78c2947d66e322407da740db3acc672c6c35
CY <onions.yang@gmail.com>
2017-04-11 13:30:04 +0800

    add IO.write class method

--- vervet.py
+++ vervet.py
@@ -42,7 +42,7 @@ class Config:
   def __is_valid_json(self, jsonstr):
     try:
       j = json.loads(jsonstr)
-    except ValueError, e:
+    except ValueError:
       return False
 
     return True
@@ -71,33 +71,48 @@ print(c.mode)
 c.update(config + ' ')
 
 # ===================================
-# deal with logging system data
+# logging system data
 # ===================================
 class IO:
+  # constant 
+  MODE_POST_SERVICE = "http://127.0.0.1:3000/data"
+  MODE_LOCAL_FILE = "data.csv"
+
+  # class variable
   mode = "standalone"
-  logging.basicConfig(filename='data.csv',level=logging.INFO)
+
+  # logging
+  logging.basicConfig(filename=MODE_LOCAL_FILE,level=logging.INFO)
 
   @classmethod
-  def write(cls):
+  def write(cls, data):
     if cls.mode == "standalone":
       logging.info("writing to %s", cls.__name__)
-      cls.__to_file()
+      cls.__to_file(data)
     else:
       logging.info("writing to %s", cls.__name__)
-      cls.__post()
+      cls.__post(data)
 
   @classmethod
-  def __post(cls):
-    #
-    print("post")
+  def __post(cls, data):
+    try:
+      r = requests.post(cls.MODE_POST_SERVICE, json=data)
+      res = r.text
+    except requests.exceptions.RequestException:
+      res = ''
+    except requests.exceptions.Timeout:
+      res = ''
+    return res
 
   @classmethod
-  def __to_file(cls):
-    print("write to file")
+  def __to_file(cls, data):
+    logging.info(data)
+    return ''
 
-# pseudo code
-# io = IO(mode)
-# io.write(data) # standalone: return ''; server: return HTTP response
-
-print(IO.write())
 print(IO.mode)
+IO.mode = "server"
+print(IO.write({"a":1, "b":2}))
+
+# ===================================
+# logging system data
+# ===================================
